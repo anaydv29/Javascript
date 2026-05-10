d5fb0aa6a5e15c5266e7b194da30e854012edca6
anaydv29 <2k23.csds2311335@gmail.com>
2026-05-10 09:54:54 +0000

    datatype summary

---
+++ 01_basis/datatypes-summary.js
@@ -0,0 +1,17 @@
+//Primitive
+// 7 types: String, Number, Boolean null, undefined,Symbol,BigInt
+const bigNumber=3465465226542566n
+
+//Reference(non primitive)
+
+//Array, Objects, Functions
+const hero=["saktiman","naagraj"]
+
+let obj={
+    name:"anamika",
+    age:20,
+}
+const myFunction=function(){
+    console.log("hello world");
+}
+

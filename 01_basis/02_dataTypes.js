0238b44d6a3e24c48de3ba33d61631a5fa367827
anaydv29 <2k23.csds2311335@gmail.com>
2026-05-10 08:55:59 +0000

    datatypes

---
+++ 01_basis/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs, not browser
+
+//in coding the very important thing is readability
+
+//number=> 2 to power 53
+//bigint
+//string =>""
+//boolean =>true/false
+//null =>standalone value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof null);

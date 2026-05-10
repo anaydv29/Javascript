00bc48d2467a4b1d1846fecc7bc409b25b065aba
anaydv29 <2k23.csds2311335@gmail.com>
2026-05-10 09:29:46 +0000

    conversion

---
+++ 01_basis/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score="33"
+
+let valueInNumber=Number(score)
+//console.log(typeof(score));
+
+//"33" => 33
+//"33abc"=> NaN
+// true =>1; false => 0
+
+let isLoggedIn=" "
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+
+//**************************Operations ********************************//
+
+console.log("1"+2+2)
+console.log(1+2+"2")
+
